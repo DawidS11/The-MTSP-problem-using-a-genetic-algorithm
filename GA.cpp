415fa6e9ac21fd8c57c93dad9743f707c7876f79
DawidS11 <dawidsmalcuga@gmail.com>
2025-02-08 19:31:20 +0100

    Add GA constructor and variables

--- GA.cpp
+++ GA.cpp
@@ -1,1 +1,5 @@
 #include "GA.hpp"
+
+GA::GA(const std::vector<Travel>& travels, size_t iterations)
+    : mTravels(travels), mIterations(iterations)
+{}
--- GA.hpp
+++ GA.hpp
@@ -2,10 +2,16 @@
 #define GA_HPP
 
 #include <iostream>
+#include "Travel.hpp"
 
 class GA
 {
+public:
+    GA(const std::vector<Travel>& travels, size_t iterations);
 
+private:
+    std::vector<Travel> mTravels;
+    size_t mIterations;
 };
 
 #endif
