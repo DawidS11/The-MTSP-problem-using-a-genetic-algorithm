f309f742c6dceb2f9e2a3bd2e950e5b587425d1c
DawidS11 <dawidsmalcuga@gmail.com>
2025-02-15 22:27:41 +0100

    Implement evolution()

--- GA.hpp
+++ GA.hpp
@@ -9,15 +9,17 @@ class GA
 {
 public:
     GA(const std::vector<Travel>& travels, size_t iterations);
-    Travel* crossoverCarterAndRagsdale2006(Travel& t1, Travel& t2);
-    Travel* crossoverStandard(Travel& t1, Travel& t2);
-    Travel* crossoverTCX(Travel& t1, Travel& t2);
+    Travel crossoverCarterAndRagsdale2006(Travel& t1, Travel& t2);
+    Travel crossoverStandard(Travel& t1, Travel& t2);
+    Travel crossoverTCX(Travel& t1, Travel& t2);
 
 private:
     std::vector<Travel> mTravels;
     size_t mIterations;
 
-    std::vector<Travel> selectParents(const Population& p);
+    std::array<Travel, 2> selectParents(const Population& p);
+    void mutation(Population& p, Travel& t);
+    void evolution(Population& p, int crossover);
 };
 
 #endif
